cd9a52eca0361d9528c3ac0d3727ddcb374e0156
Thang Cao <cvthang56th2@gmail.com>
2025-10-27 23:32:38 +0700

    keep original form

--- src/problem2/src/App.tsx
+++ src/problem2/src/App.tsx
@@ -1,10 +1,18 @@
-import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <>
+      <form>
+        <h5>Swap</h5>
+        <label htmlFor="input-amount">Amount to send</label>
+        <input id="input-amount" />
+
+        <label htmlFor="output-amount">Amount to receive</label>
+        <input id="output-amount" />
+
+        <button>CONFIRM SWAP</button>
+      </form>
+    </>
   )
 }
 
